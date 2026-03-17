a988022cda438e7e8060fdfa4bf85147a9dec489
wonderhyenstudy <wonderhyenstudy@gmail.com>
2026-03-17 17:56:46 +0900

    feat : 시간(생성, 수정) 엔티티 추가, 시간 자동 관리 기능 활성화, 멤버 엔티티 추가

--- src/main/java/com/library/project/library/LibraryApplication.java
+++ src/main/java/com/library/project/library/LibraryApplication.java
@@ -2,8 +2,11 @@ package com.library.project.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+// 생성 시간, 수정 시간, 작성자 등을 자동으로 관리해주는 JPA Auditing 기능을 활성화하는 어노테이션
+@EnableJpaAuditing
 public class LibraryApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/library/project/library/entity/Member.java
@@ -0,0 +1,45 @@
+package com.library.project.library.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 내부 PK
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String mid; // 로그인 아이디
+
+    @Column(length = 100, nullable = false)
+    private String mpw; // 비밀번호
+
+    @Column(length = 50, nullable = false)
+    private String mname; // 이름
+
+    private String email;
+    private String region;
+
+    @Enumerated(EnumType.STRING)
+    private Role role; // [USER, ADMIN]
+
+    public enum Role {
+        USER, ADMIN
+    }
+
+    // 눈에 보이지 않지만, BaseEntity를 이용해서, regDate, modDate 도 추가가 될 예정.
+
+    // 정보 수정 메서드 (내 서재/마이페이지용)
+    public void change(String mname, String email, String region) {
+        this.mname = mname;
+        this.email = email;
+        this.region = region;
+    }
+}
