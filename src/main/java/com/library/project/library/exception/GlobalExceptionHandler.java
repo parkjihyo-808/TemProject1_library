0349007dbdcb3ca826f22b5036157ef8f1054ca0
JOLDO <bbat115@gmail.com>
2026-03-21 16:55:37 +0900

    chore: 머시 할때 같은 파일 있어서 수정

--- src/main/java/com/library/project/library/exception/GlobalExceptionHandler.java
+++ src/main/java/com/library/project/library/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.library.project.library.exception;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,5 +15,11 @@ public class GlobalExceptionHandler {
         org.springframework.http.HttpHeaders headers = new org.springframework.http.HttpHeaders();
         headers.add("Content-Type", "text/plain;charset=UTF-8");
         return new ResponseEntity<>(body, headers, org.springframework.http.HttpStatus.BAD_REQUEST);
+    }
+
+    // NotLoginException 발생 시 401 응답 반환
+    @ExceptionHandler(NotLoginException.class)
+    public ResponseEntity<String> handleNotLogin(NotLoginException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 }
